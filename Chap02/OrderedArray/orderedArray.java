3add47f2b2364f9696bef62c758010d2e57e7d9b
Brad Ellis <bradellis10@gmail.com>
2019-03-24 22:11:31 -0700

    fix field names in OrderedArray

--- Chap02/OrderedArray/orderedArray.java
+++ Chap02/OrderedArray/orderedArray.java
@@ -2,72 +2,74 @@
 // demonstrates ordered array class
 // to run this program: C>java OrderedApp
 ////////////////////////////////////////////////////////////////
+@SuppressWarnings("Duplicates")
 class OrdArray
    {
    private long[] a;                 // ref to array a
-   private int nElems;               // number of data items
+   private int nElements;               // number of data items
    //-----------------------------------------------------------
    public OrdArray(int max)          // constructor
       {
       a = new long[max];             // create array
-      nElems = 0;
+      nElements = 0;
       }
    //-----------------------------------------------------------
    public int size()
-      { return nElems; }
+      { return nElements; }
    //-----------------------------------------------------------
    public int find(long searchKey)
       {
       int lowerBound = 0;
-      int upperBound = nElems-1;
-      int curIn;
+      int upperBound = nElements-1;
+      int currentIndex;
 
       while(true)
          {
-         curIn = (lowerBound + upperBound ) / 2;
-         if(a[curIn]==searchKey)
-            return curIn;              // found it
+         currentIndex = (lowerBound + upperBound ) / 2;
+         if(a[currentIndex]==searchKey)
+            return currentIndex;              // found it
          else if(lowerBound > upperBound)
-            return nElems;             // can't find it
+            return nElements;             // can't find it
          else                          // divide range
             {
-            if(a[curIn] < searchKey)
-               lowerBound = curIn + 1; // it's in upper half
+            if(a[currentIndex] < searchKey)
+               lowerBound = currentIndex + 1; // it's in upper half
             else
-               upperBound = curIn - 1; // it's in lower half
+               upperBound = currentIndex - 1; // it's in lower half
             }  // end else divide range
          }  // end while
       }  // end find()
    //-----------------------------------------------------------
    public void insert(long value)    // put element into array
       {
+
       int j;
-      for(j=0; j<nElems; j++)        // find where it goes
+      for(j=0; j<nElements; j++)        // find where it goes
          if(a[j] > value)            // (linear search)
             break;
-      for(int k=nElems; k>j; k--)    // move bigger ones up
+      for(int k=nElements; k>j; k--)    // move bigger ones up
          a[k] = a[k-1];
       a[j] = value;                  // insert it
-      nElems++;                      // increment size
+      nElements++;                      // increment size
       }  // end insert()
    //-----------------------------------------------------------
    public boolean delete(long value)
       {
       int j = find(value);
-      if(j==nElems)                  // can't find it
+      if(j==nElements)                  // can't find it
          return false;
       else                           // found it
          {
-         for(int k=j; k<nElems; k++) // move bigger ones down
+         for(int k=j; k<nElements; k++) // move bigger ones down
             a[k] = a[k+1];
-         nElems--;                   // decrement size
+         nElements--;                   // decrement size
          return true;
          }
       }  // end delete()
    //-----------------------------------------------------------
    public void display()             // displays array contents
       {
-      for(int j=0; j<nElems; j++)       // for each element,
+      for(int j=0; j<nElements; j++)       // for each element,
          System.out.print(a[j] + " ");  // display it
       System.out.println("");
       }
